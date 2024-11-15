8ddb724f41537f7ac2fa43ffffcea242521d63d7
Ryldi <reynaldi.adidarma@binus.ac.id>
2024-11-15 21:14:42 +0700

    chore: add manifest.json

--- database/migrations/2024_11_13_080100_create_students_table.php
+++ database/migrations/2024_11_13_080100_create_students_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('password');
             $table->string('username');
             $table->integer('phone');
-            $table->string('address');
+            $table->string('address')->nullable();
             $table->timestamps();
         });
     }
